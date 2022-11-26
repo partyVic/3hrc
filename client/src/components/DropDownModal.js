bc81f8e4281144ac808241c90cd69e709a5aab3c
partyVic <vicwang128@gmail.com>
2022-11-26 23:48:20 +1100

    Clean Up Navbar items

--- client/src/components/DropDownModal.js
+++ client/src/components/DropDownModal.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './dropDownModal.css'
-import DropDownModalItems from './DropDownModalItems'
 
 const DropDownModal = ({ navItems }) => {
+
   return (
     <div className='DropDownModal pt-4 w-56 h-24 rounded-b-md'>
       {navItems.map(item => (
         <div
-          className={`ml-8 mt-2`}
+          className={`ml-8 mt-2 hover:text-red-500 hover:underline hover:underline-offset-2 hover:decoration-solid hover:cursor-pointer`}
           key={item}
         >
-          <DropDownModalItems title={item} />
+          {item}
         </div>
       ))}
     </div>
