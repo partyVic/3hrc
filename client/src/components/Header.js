90a91f3a4afc78f14204968f16abab65e56c4ca6
partyVic <vicwang128@gmail.com>
2022-11-28 00:08:37 +1100

    Working on header title

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import NavBar from './navBar/NavBar'
+import HeaderTitle from './headerTitle/HeaderTitle'
 
 const Header = () => {
   return (
     <div
       className='h-28'
     >
-      <div className='h-1/2 border-b-2 border-[#f5f5f5] font-mono'>3HCR</div>
+      <div className='h-1/2 border-b-2 border-[#f5f5f5] font-mono'>
+        <HeaderTitle />
+      </div>
+      
       <div className='h-1/2 border-b-2 border-[#f5f5f5] font-serif'>
         <NavBar />
       </div>
---
+++ client/src/components/headerTitle/HeaderTitle.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const headerTitle = [
+    { title: '3HCR' },
+    { title: 'Radio' },
+]
+
+const headerItems = [
+    { item: 'About us' },
+    { item: 'Help & support' }
+]
+
+const HeaderTitle = () => {
+    return (
+        <div>3HCR</div>
+    )
+}
+
+export default HeaderTitle
