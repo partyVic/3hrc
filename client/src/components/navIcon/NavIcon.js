7e19e7432b9d06d2be89a5704ef1d63d0c3c292d
partyVic <vicwang128@gmail.com>
2022-11-30 13:00:34 +1100

    Some bug fixed & Add click outside hook

--- client/src/components/navIcon/NavIcon.js
+++ client/src/components/navIcon/NavIcon.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import Bars from '../icons/bars'
 import NavIconModal from './navIconModal'
 
-const navBarItems = [
-  { title: 'Home', navItems: [], navItemURL: '/' },
-  { title: 'Infomation', navItems: [], navItemURL: 'info' },
-  { title: 'Programs', navItems: ['Program 1', 'Program 2'], navItemURL: 'programs' },
-  { title: 'Forms', navItems: ['Form 1', 'From2'], navItemURL: '' },
-  { title: 'Sponsors', navItems: [], navItemURL: 'sponsors' },
-]
+// const navBarItems = [
+//   { title: 'Home', navItems: [], navItemURL: '/' },
+//   { title: 'Infomation', navItems: [], navItemURL: 'info' },
+//   { title: 'Programs', navItems: ['Program 1', 'Program 2'], navItemURL: 'programs' },
+//   { title: 'Forms', navItems: ['Form 1', 'From2'], navItemURL: '' },
+//   { title: 'Sponsors', navItems: [], navItemURL: 'sponsors' },
+// ]
 
 const NavIcon = () => {
   const [isOpenModal, setIsOpenModal] = useState(false)
--- client/src/components/navIcon/navIconModal.js
+++ client/src/components/navIcon/navIconModal.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import XMark from '../icons/x-mark'
+import useOutSideClick from '../../helperFunctions/useOutSideClick'
 
 const NavIconModal = ({ handleModalClose }) => {
+    const ref = useRef()
+
+    // Call hook passing in the ref and a function to call on outside click
+    useOutSideClick(ref, handleModalClose)
+
     return (
-        <div className='flex'>
-            <div className='w-1/2'>
+        <div className='flex' >
+            <div className='w-3/4' ref={ref}>
                 <div className='h-10 bg-black'></div>
                 <div className='h-96 bg-neutral-100'></div>
             </div>
             <div
-                className='mt-2 ml-2 cursor-pointer'
+                className='mt-2 ml-2 h-10 cursor-pointer'
                 onClick={handleModalClose}
             >
                 <XMark />
