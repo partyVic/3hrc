a49dd6376bf2417dcacbb5bac2091af978ff9df8
partyVic <vicwang128@gmail.com>
2022-12-01 20:17:16 +1100

    Audio style touch up

--- client/src/components/radio/Radio.js
+++ client/src/components/radio/Radio.js
@@ -4,26 +4,30 @@ import 'react-h5-audio-player/lib/styles.css';
 import './radio.css'
 import AUDIO_URL from '../../dataBase/audioUrl'
 
-
+// player
+// md:h-2/3 md:top-7 md:left-32 md:relative
+// h-full
 const Radio = () => {
     return (
         <div className='
-            md:w-2/5
+            md:w-full md:flex md:justify-center md:items-center
             h-full
         '>
-            <AudioPlayer
-                className="
-                player
-                md:h-2/3
-                h-full
-                "
-                src={AUDIO_URL}
-                showJumpControls={false}
-                layout="stacked"
-                customProgressBarSection={[]}
-                customControlsSection={["MAIN_CONTROLS", "VOLUME_CONTROLS"]}
-                autoPlayAfterSrcChange={false}
-            />
+            <div className='
+            md:w-3/5 md:h-2/3
+            h-full
+            '>
+
+                <AudioPlayer
+                    className="player"
+                    src={AUDIO_URL}
+                    showJumpControls={false}
+                    layout="stacked"
+                    customProgressBarSection={[]}
+                    customControlsSection={["MAIN_CONTROLS", "VOLUME_CONTROLS"]}
+                    autoPlayAfterSrcChange={false}
+                />
+            </div>
         </div>
     )
 }
