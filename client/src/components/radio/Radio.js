cbc21449044a53588958b223f3413ac4d3998fe1
partyVic <vicwang128@gmail.com>
2022-12-01 19:26:46 +1100

    Working on Audio streaming

--- client/src/components/radio/Radio.js
+++ client/src/components/radio/Radio.js
@@ -1,15 +1,29 @@
 import React from 'react'
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import './radio.css'
 import AUDIO_URL from '../../dataBase/audioUrl'
+
 
 const Radio = () => {
     return (
-        <div>
-
-            <audio
-                
-                controls
-                src={AUDIO_URL}>
-            </audio>
+        <div className='
+            md:w-2/5
+            h-full
+        '>
+            <AudioPlayer
+                className="
+                player
+                md:h-2/3
+                h-full
+                "
+                src={AUDIO_URL}
+                showJumpControls={false}
+                layout="stacked"
+                customProgressBarSection={[]}
+                customControlsSection={["MAIN_CONTROLS", "VOLUME_CONTROLS"]}
+                autoPlayAfterSrcChange={false}
+            />
         </div>
     )
 }
