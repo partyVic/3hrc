bae0f8581ae5270bc4362d1fc7dc5b9c3eb40352
partyVic <vicwang128@gmail.com>
2022-12-07 21:44:18 +1100

    Working on emailjs

--- client/src/screens/Support.js
+++ client/src/screens/Support.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import Spinner from '../components/icons/spinner';
 
-const Support = () => {
+export const Support = () => {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID, process.env.REACT_APP_EMAILJS_TEMPLATE_ID, form.current, process.env.REACT_APP_EMAILJS_PUBLIC_KEY)
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
-    <div>Support</div>
-  )
-}
+    <div>
+      <Spinner />
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <button type='submit' disabled>Send</button>
+      </form>
+    </div>
+  );
+};
 
 export default Support
