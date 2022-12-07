41859276b4f062a50da6d7e62c680648e0e2262a
partyVic <vicwang128@gmail.com>
2022-12-07 22:56:46 +1100

    Working on Support screen

--- client/src/screens/Support.js
+++ client/src/screens/Support.js
@@ -5,6 +5,8 @@ import Spinner from '../components/icons/spinner';
 export const Support = () => {
   const form = useRef()
 
+  const [isSending, setIsSending] = useState(false)
+
   const sendEmail = (e) => {
     e.preventDefault();
 
@@ -18,16 +20,86 @@ export const Support = () => {
 
   return (
     <div>
-      <Spinner />
       <form ref={form} onSubmit={sendEmail}>
         <label>Name</label>
-        <input type="text" name="user_name" />
+        <input type="text" name="user_name" required />
         <label>Email</label>
-        <input type="email" name="user_email" />
+        <input type="email" name="user_email" required />
         <label>Message</label>
-        <textarea name="message" />
-        <button type='submit' disabled>Send</button>
+        <textarea name="message" required />
+        <button type='submit'>Send</button>
       </form>
+
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+
+          </div>
+
+          <form className="mt-8 space-y-6">
+            <div className="-space-y-px rounded-md shadow-sm">
+
+              <div>
+                <label htmlFor="name" className="sr-only">
+                  Name
+                </label>
+                <input
+                  id="name"
+                  name="name"
+                  type="name"
+                  autoComplete="current-name"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Name"
+                />
+              </div>
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="message" className="sr-only">
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  type="message"
+                  autoComplete="current-message"
+                  rows={6}
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Add your message..."
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                </span>
+                {!isSending ? 'Send' : <Spinner />}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
