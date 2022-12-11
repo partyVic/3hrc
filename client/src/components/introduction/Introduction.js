02286823a6a1a1306ad8f4e6e1d7a597af109fc1
partyVic <vicwang128@gmail.com>
2022-12-11 22:44:55 +1100

    some style changes

--- client/src/components/introduction/Introduction.js
+++ client/src/components/introduction/Introduction.js
@@ -14,7 +14,7 @@ const Introduction = () => {
                 md:hidden
                 h-1/5 flex
                 '>
-                <div className='h-4/5 w-1 bg-rose-500 rounded ml-4 mt-2.5'></div>
+                <div className='h-4/5 w-1 bg-rose-500 ml-4 mt-2.5'></div>
                 <div className='w-full h-full mt-2 mx-4'>
                     <h2 className='text-lg font-bold text-rose-500'>{INTRO_TITLE}</h2>
                     <p className='mt-1 text-sm font-medium text-slate-500'>{INTRO_BODY}</p>
@@ -36,7 +36,7 @@ const Introduction = () => {
                     md:absolute md:flex md:h-1/4 md:w-2/5 md:bg-[rgba(255,255,255,0.4)] md:top-80 md:left-14 md:rounded-xl backdrop-opacity-10 backdrop-invert bg-white/30
                     hidden
                     '>
-                    <div className='h-4/5 w-1 bg-rose-500 rounded ml-4 mt-6'></div>
+                    <div className='h-4/5 w-1 bg-rose-500 ml-4 mt-6'></div>
                     <div className='w-full h-full mt-4 ml-10 mr-4'>
                         <h2 className='text-4xl font-bold text-rose-500'>{INTRO_TITLE}</h2>
                         <p className='mt-4 text-lg font-medium text-white'>{INTRO_BODY}</p>
--- client/src/components/news/NewsBoard.js
+++ client/src/components/news/NewsBoard.js
@@ -16,12 +16,13 @@ const NewsBoard = () => {
             </div>
             <div className="relative max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
                 <div className="md:ml-auto md:w-1/2 md:pl-10">
-                    <h2 className="text-base font-semibold uppercase tracking-wider text-gray-300">Award winning support</h2>
-                    <p className="mt-2 text-white text-3xl font-extrabold tracking-tight sm:text-4xl">We’re here to help</p>
-                    <p className="mt-3 text-lg text-gray-300">
+                    <div className='flex'>
+                        <span className=' bg-rose-500 w-1 mr-4'></span>
+                        <p className="text-rose-500 text-3xl font-extrabold tracking-tight sm:text-4xl">News</p>
+                    </div>
+                    <p className="mt-3 text-lg text-gray-500">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a
-                        scelerisque amet ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat quisque ut interdum
-                        tincidunt duis.
+                        scelerisque amet ullamcorper eu enim et fermentum, augue.
                     </p>
                     <div className="mt-8">
                         <div className="inline-flex rounded-md shadow">
@@ -29,7 +30,7 @@ const NewsBoard = () => {
                                 onClick={() => navigate('/news')}
                                 className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white hover:text-[rgba(255,255,255,0.8)] bg-rose-500 hover:bg-rose-600 cursor-pointer"
                             >
-                                Visit the help center
+                                Discover news channel
                             </span>
                         </div>
                     </div>
