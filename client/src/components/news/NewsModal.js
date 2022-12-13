d56929e13e020997e732e89ce37139b19e9b0be4
partyVic <vicwang128@gmail.com>
2022-12-13 22:56:16 +1100

    working on news

--- client/src/components/news/NewsModal.js
+++ client/src/components/news/NewsModal.js
@@ -5,12 +5,23 @@ import img from '../../assets/images/news-title.png'
 const NewsModal = () => {
     return (
         <div className='
-        flex
+        mx-4 my-4
+        md:mx-12 lg:mx-40 xl:mx-72
         '>
-            <img className='w-[20%]' src={img} alt="" />
-            <div>
-                <span>time</span>
-                <p>content</p>
+            <div className='bg-rose-500 h-1 w-full mb-4 relative'>
+                <div className='text-sm font-bold text-gray-500 absolute bg-white -top-2 pr-4'>
+                    12-12-2022
+                </div>
+            </div>
+
+            <div className='flex bg-[#f5f5f5] p-2 rounded'>
+                <img
+                    className='lg:w-[20%] md:w-[25%] w-[35%] rounded-l'
+                    src={img} alt="" />
+                <div>
+                    <p>content</p>
+                </div>
+
             </div>
         </div>
     )
